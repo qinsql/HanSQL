f436a2cdf65a9374141cbf8ebc57b042417f1736
Honghua Zhu <zhh200910@gmail.com>
2021-08-15 13:26:18 +0800

    删除mutable

--- hansql-optimizer/src/main/java/org/apache/calcite/rel/rules/AggregateFilterTransposeRule.java
+++ hansql-optimizer/src/main/java/org/apache/calcite/rel/rules/AggregateFilterTransposeRule.java
@@ -16,11 +16,13 @@
  */
 package org.apache.calcite.rel.rules;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.apache.calcite.plan.RelOptRule;
 import org.apache.calcite.plan.RelOptRuleCall;
 import org.apache.calcite.plan.RelOptRuleOperand;
 import org.apache.calcite.plan.RelOptUtil;
-import org.apache.calcite.plan.SubstitutionVisitor;
 import org.apache.calcite.rel.RelNode;
 import org.apache.calcite.rel.core.Aggregate;
 import org.apache.calcite.rel.core.Aggregate.Group;
@@ -31,14 +33,12 @@ import org.apache.calcite.rel.metadata.RelMetadataQuery;
 import org.apache.calcite.rex.RexNode;
 import org.apache.calcite.rex.RexUtil;
 import org.apache.calcite.sql.SqlAggFunction;
+import org.apache.calcite.sql.fun.SqlStdOperatorTable;
 import org.apache.calcite.tools.RelBuilderFactory;
 import org.apache.calcite.util.ImmutableBitSet;
 import org.apache.calcite.util.mapping.Mappings;
 
 import com.google.common.collect.ImmutableList;
-
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * Planner rule that matches an {@link org.apache.calcite.rel.core.Aggregate}
@@ -56,105 +56,98 @@ import java.util.List;
  * @see org.apache.calcite.rel.rules.FilterAggregateTransposeRule
  */
 public class AggregateFilterTransposeRule extends RelOptRule {
-  public static final AggregateFilterTransposeRule INSTANCE =
-      new AggregateFilterTransposeRule();
+    public static final AggregateFilterTransposeRule INSTANCE = new AggregateFilterTransposeRule();
 
-  private AggregateFilterTransposeRule() {
-    this(
-        operand(Aggregate.class,
-            operand(Filter.class, any())),
-        RelFactories.LOGICAL_BUILDER);
-  }
-
-  /** Creates an AggregateFilterTransposeRule. */
-  public AggregateFilterTransposeRule(RelOptRuleOperand operand,
-      RelBuilderFactory relBuilderFactory) {
-    super(operand, relBuilderFactory, null);
-  }
-
-  public void onMatch(RelOptRuleCall call) {
-    final Aggregate aggregate = call.rel(0);
-    final Filter filter = call.rel(1);
-
-    // Do the columns used by the filter appear in the output of the aggregate?
-    final ImmutableBitSet filterColumns =
-        RelOptUtil.InputFinder.bits(filter.getCondition());
-    final ImmutableBitSet newGroupSet =
-        aggregate.getGroupSet().union(filterColumns);
-    final RelNode input = filter.getInput();
-    final RelMetadataQuery mq = call.getMetadataQuery();
-    final Boolean unique = mq.areColumnsUnique(input, newGroupSet);
-    if (unique != null && unique) {
-      // The input is already unique on the grouping columns, so there's little
-      // advantage of aggregating again. More important, without this check,
-      // the rule fires forever: A-F => A-F-A => A-A-F-A => A-A-A-F-A => ...
-      return;
+    private AggregateFilterTransposeRule() {
+        this(operand(Aggregate.class, operand(Filter.class, any())), RelFactories.LOGICAL_BUILDER);
     }
-    boolean allColumnsInAggregate = aggregate.getGroupSet().
-        contains(filterColumns);
-    final Aggregate newAggregate =
-        aggregate.copy(aggregate.getTraitSet(), input,
-                false, newGroupSet, null, aggregate.getAggCallList());
-    final Mappings.TargetMapping mapping = Mappings.target(
-        newGroupSet::indexOf,
-        input.getRowType().getFieldCount(),
-        newGroupSet.cardinality());
-    final RexNode newCondition =
-        RexUtil.apply(mapping, filter.getCondition());
-    final Filter newFilter = filter.copy(filter.getTraitSet(),
-        newAggregate, newCondition);
-    if (allColumnsInAggregate && aggregate.getGroupType() == Group.SIMPLE) {
-      // Everything needed by the filter is returned by the aggregate.
-      assert newGroupSet.equals(aggregate.getGroupSet());
-      call.transformTo(newFilter);
-    } else {
-      // If aggregate uses grouping sets, we always need to split it.
-      // Otherwise, it means that grouping sets are not used, but the
-      // filter needs at least one extra column, and now aggregate it away.
-      final ImmutableBitSet.Builder topGroupSet = ImmutableBitSet.builder();
-      for (int c : aggregate.getGroupSet()) {
-        topGroupSet.set(newGroupSet.indexOf(c));
-      }
-      ImmutableList<ImmutableBitSet> newGroupingSets = null;
-      if (aggregate.getGroupType() != Group.SIMPLE) {
-        ImmutableList.Builder<ImmutableBitSet> newGroupingSetsBuilder =
-                ImmutableList.builder();
-        for (ImmutableBitSet groupingSet : aggregate.getGroupSets()) {
-          final ImmutableBitSet.Builder newGroupingSet =
-                  ImmutableBitSet.builder();
-          for (int c : groupingSet) {
-            newGroupingSet.set(newGroupSet.indexOf(c));
-          }
-          newGroupingSetsBuilder.add(newGroupingSet.build());
-        }
-        newGroupingSets = newGroupingSetsBuilder.build();
-      }
-      final List<AggregateCall> topAggCallList = new ArrayList<>();
-      int i = newGroupSet.cardinality();
-      for (AggregateCall aggregateCall : aggregate.getAggCallList()) {
-        final SqlAggFunction rollup =
-            SubstitutionVisitor.getRollup(aggregateCall.getAggregation());
-        if (rollup == null) {
-          // This aggregate cannot be rolled up.
-          return;
-        }
-        if (aggregateCall.isDistinct()) {
-          // Cannot roll up distinct.
-          return;
-        }
-        topAggCallList.add(
-            AggregateCall.create(rollup, aggregateCall.isDistinct(),
-                aggregateCall.isApproximate(), ImmutableList.of(i++), -1,
-                aggregateCall.collation,
-                aggregateCall.type, aggregateCall.name));
-      }
-      final Aggregate topAggregate =
-          aggregate.copy(aggregate.getTraitSet(), newFilter,
-              aggregate.indicator, topGroupSet.build(),
-              newGroupingSets, topAggCallList);
-      call.transformTo(topAggregate);
+
+    /** Creates an AggregateFilterTransposeRule. */
+    public AggregateFilterTransposeRule(RelOptRuleOperand operand, RelBuilderFactory relBuilderFactory) {
+        super(operand, relBuilderFactory, null);
     }
-  }
+
+    @Override
+    public void onMatch(RelOptRuleCall call) {
+        final Aggregate aggregate = call.rel(0);
+        final Filter filter = call.rel(1);
+
+        // Do the columns used by the filter appear in the output of the aggregate?
+        final ImmutableBitSet filterColumns = RelOptUtil.InputFinder.bits(filter.getCondition());
+        final ImmutableBitSet newGroupSet = aggregate.getGroupSet().union(filterColumns);
+        final RelNode input = filter.getInput();
+        final RelMetadataQuery mq = call.getMetadataQuery();
+        final Boolean unique = mq.areColumnsUnique(input, newGroupSet);
+        if (unique != null && unique) {
+            // The input is already unique on the grouping columns, so there's little
+            // advantage of aggregating again. More important, without this check,
+            // the rule fires forever: A-F => A-F-A => A-A-F-A => A-A-A-F-A => ...
+            return;
+        }
+        boolean allColumnsInAggregate = aggregate.getGroupSet().contains(filterColumns);
+        final Aggregate newAggregate = aggregate.copy(aggregate.getTraitSet(), input, false, newGroupSet, null,
+                aggregate.getAggCallList());
+        final Mappings.TargetMapping mapping = Mappings.target(newGroupSet::indexOf, input.getRowType().getFieldCount(),
+                newGroupSet.cardinality());
+        final RexNode newCondition = RexUtil.apply(mapping, filter.getCondition());
+        final Filter newFilter = filter.copy(filter.getTraitSet(), newAggregate, newCondition);
+        if (allColumnsInAggregate && aggregate.getGroupType() == Group.SIMPLE) {
+            // Everything needed by the filter is returned by the aggregate.
+            assert newGroupSet.equals(aggregate.getGroupSet());
+            call.transformTo(newFilter);
+        } else {
+            // If aggregate uses grouping sets, we always need to split it.
+            // Otherwise, it means that grouping sets are not used, but the
+            // filter needs at least one extra column, and now aggregate it away.
+            final ImmutableBitSet.Builder topGroupSet = ImmutableBitSet.builder();
+            for (int c : aggregate.getGroupSet()) {
+                topGroupSet.set(newGroupSet.indexOf(c));
+            }
+            ImmutableList<ImmutableBitSet> newGroupingSets = null;
+            if (aggregate.getGroupType() != Group.SIMPLE) {
+                ImmutableList.Builder<ImmutableBitSet> newGroupingSetsBuilder = ImmutableList.builder();
+                for (ImmutableBitSet groupingSet : aggregate.getGroupSets()) {
+                    final ImmutableBitSet.Builder newGroupingSet = ImmutableBitSet.builder();
+                    for (int c : groupingSet) {
+                        newGroupingSet.set(newGroupSet.indexOf(c));
+                    }
+                    newGroupingSetsBuilder.add(newGroupingSet.build());
+                }
+                newGroupingSets = newGroupingSetsBuilder.build();
+            }
+            final List<AggregateCall> topAggCallList = new ArrayList<>();
+            int i = newGroupSet.cardinality();
+            for (AggregateCall aggregateCall : aggregate.getAggCallList()) {
+                final SqlAggFunction rollup = getRollup(aggregateCall.getAggregation());
+                if (rollup == null) {
+                    // This aggregate cannot be rolled up.
+                    return;
+                }
+                if (aggregateCall.isDistinct()) {
+                    // Cannot roll up distinct.
+                    return;
+                }
+                topAggCallList.add(AggregateCall.create(rollup, aggregateCall.isDistinct(),
+                        aggregateCall.isApproximate(), ImmutableList.of(i++), -1, aggregateCall.collation,
+                        aggregateCall.type, aggregateCall.name));
+            }
+            final Aggregate topAggregate = aggregate.copy(aggregate.getTraitSet(), newFilter, aggregate.indicator,
+                    topGroupSet.build(), newGroupingSets, topAggCallList);
+            call.transformTo(topAggregate);
+        }
+    }
+
+    public static SqlAggFunction getRollup(SqlAggFunction aggregation) {
+        if (aggregation == SqlStdOperatorTable.SUM || aggregation == SqlStdOperatorTable.MIN
+                || aggregation == SqlStdOperatorTable.MAX || aggregation == SqlStdOperatorTable.SUM0
+                || aggregation == SqlStdOperatorTable.ANY_VALUE) {
+            return aggregation;
+        } else if (aggregation == SqlStdOperatorTable.COUNT) {
+            return SqlStdOperatorTable.SUM0;
+        } else {
+            return null;
+        }
+    }
 }
 
 // End AggregateFilterTransposeRule.java
